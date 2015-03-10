70d4c494121b42e9a69cd0470b984f8ba648cfda
jwEric <jwchengm@gmail.com>
2015-03-10 15:19:50 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 3,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/jwEric/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145170,
+  277980,
+  821,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T07:14:36.000Z",
+  "2015-03-10T07:14:36.000Z",
+  "2015-03-10T07:14:36.000Z",
+  "2015-03-10T07:19:00.000Z",
+  "2015-03-10T07:14:36.000Z",
+  "2015-03-10T07:15:08.000Z",
+  "2015-03-10T07:14:36.000Z",
+  "2015-03-10T07:14:36.000Z",
+  "2015-03-10T07:14:52.000Z"
+ ],
+ "date": "2015-03-10T07:18:59.604Z"
+})
